34f2d422ba9d2bcfc9f22c1ed933f347fe272ed7
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-20 17:22:01 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.get("/", (req, res) => {
+    res.send("App is working!")
+    
+});
+
+
+
+
+
+app.listen(port, () => {
+    console.log("App listening");
+})
+
