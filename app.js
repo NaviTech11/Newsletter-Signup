26955e39a565d61bbf63134d93eb54752f735cc4
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-21 11:30:32 -0500

    POST data on Mailchimp's server via their API & hide API keys

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+ 
+require("dotenv").config(); 
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
@@ -14,13 +17,46 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const apiKey = process.env.API_KEY;
+    const audienceId = process.env.AUDIENCE_ID;
+    const server = process.env.SERVER;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = `https://${server}.api.mailchimp.com/3.0/lists/${audienceId}`
+
+    const options = {
+        method: "POST",
+        auth: `ivan1:${apiKey}`
+    }
+
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) =>{
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
 });
-
 
 
 
@@ -28,4 +64,6 @@ app.post("/", (req, res) => {
 app.listen(port, () => {
     console.log("App listening");
 });
+
+
 
